46e0aa7c7978262d021e9744c1ef1e53cc87d227
Orest Matsukh <orest.sd@gmail.com>
2024-01-27 18:29:04 +0200

    add

--- themes/64ai/assets/js/script.js
+++ themes/64ai/assets/js/script.js
@@ -1,115 +1,113 @@
-(function () {
-  const mobileMenuBreakpoint = 991;
-  let winW = null;
+window.addEventListener('load', handleWindowResize);
+window.addEventListener('resize', handleWindowResize);
 
-  ["load", "resize"].forEach((event) => {
-    window.addEventListener(event, () => {
-      calcWinSizes();
-      resizeMenu();
+function handleWindowResize() {
+  const windowW = window.innerWidth;
+  const overlay = document.querySelector(".sixtyia-overlay");
+  const header = document.querySelector('.sixtyia-header');
+  const subMenu = document.querySelector('.sub-menu');
+
+  if (windowW >= 992) {
+    overlay.classList.remove('active');
+    header.classList.remove('menu-open');
+    subMenu.removeAttribute('style');
+  }
+}
+
+const btn = document.querySelectorAll('.sixtyia-header .header-menu > .menu-item-has-children');
+
+if (btn) {
+  btn.forEach(function (button) {
+    const menu = button.querySelector('.sub-menu');
+
+    button.addEventListener('click', function () {
+      const windowW = window.innerWidth;
+
+      if (windowW <= 992) {
+        if (menu.style.display === 'none' || menu.style.display === '') {
+          menu.style.display = 'block';
+        } else {
+          menu.style.display = 'none';
+        }
+      }
     });
   });
+}
 
 
 
-  if (document.querySelector(".sixtyia-header")) {
-    const headerEl = document.querySelector(".sixtyia-header");
-    const htmlEl = document.querySelector("html");
-    const burger = document.querySelector(".sixtyia-header__burger");
-    const overlay = document.querySelector('.sixtyia-overlay');
+const mobileMenuBreakpoint = 991;
+let winW = null;
 
-    if (burger) {
-      burger.addEventListener("click", function () {
-        this.classList.toggle("active");
+["load", "resize"].forEach((event) => {
+  window.addEventListener(event, () => {
+    calcWinSizes();
+    resizeMenu();
+  });
+});
 
-        if (this.classList.contains("active")) {
-          htmlEl.classList.add("no-scroll");
-          headerEl.classList.add("menu-open");
-          overlay.classList.add('active');
-        } else {
-          overlay.classList.remove('active');
-          htmlEl.classList.remove("no-scroll");
-          headerEl.classList.remove("menu-open");
-        }
-      });
-    }
+
+
+if (document.querySelector(".sixtyia-header")) {
+  const headerEl = document.querySelector(".sixtyia-header");
+  const htmlEl = document.querySelector("html");
+  const burger = document.querySelector(".sixtyia-header__burger");
+  const overlay = document.querySelector('.sixtyia-overlay');
+
+  if (burger) {
+    burger.addEventListener("click", function () {
+      this.classList.toggle("active");
+
+      if (this.classList.contains("active")) {
+        htmlEl.classList.add("no-scroll");
+        headerEl.classList.add("menu-open");
+        overlay.classList.add('active');
+      } else {
+        overlay.classList.remove('active');
+        htmlEl.classList.remove("no-scroll");
+        headerEl.classList.remove("menu-open");
+      }
+    });
   }
+}
 
-  function calcWinSizes() {
-    winW = window.innerWidth;
+function calcWinSizes() {
+  winW = window.innerWidth;
+}
+
+function resizeMenu() {
+  if (
+    window.screen.width > mobileMenuBreakpoint &&
+    document.querySelector("html").classList.contains("no-scroll") &&
+    document.querySelector(".sixtyia-header__burger") !== null
+  ) {
+    document.querySelector("html").classList.remove("no-scroll");
+    document
+      .querySelector(".sixtyia-header__burger")
+      .classList.toggle("active");
   }
-
-  function resizeMenu() {
-    if (
-      window.screen.width > mobileMenuBreakpoint &&
-      document.querySelector("html").classList.contains("no-scroll") &&
-      document.querySelector(".sixtyia-header__burger") !== null
-    ) {
-      document.querySelector("html").classList.remove("no-scroll");
-      document
-        .querySelector(".sixtyia-header__burger")
-        .classList.toggle("active");
-    }
-  }
+}
 
 
 
-  const headerLink = document.querySelectorAll('a[href="#"]');
+const headerLink = document.querySelectorAll('a[href="#"]');
 
-  if (headerLink) {
-    headerLink.forEach(function (el) {
-      el.addEventListener('click', function (el) {
-        el.preventDefault();
-      })
+if (headerLink) {
+  headerLink.forEach(function (el) {
+    el.addEventListener('click', function (el) {
+      el.preventDefault();
     })
-  }
-
-
-})();
-
-
-
-
-(function ($, window, document, undefined) {
-  'use strict';
-
-  $(window).on('load resize', function () {
-    const windowW = $(this).innerWidth();
-    const overlay = $(".sixtyia-overlay");
-    const header = $('.sixtyia-header');
-    const subMenu = $('.sub-menu');
-
-    if (windowW >= 992) {
-      overlay.removeClass('active');
-      header.removeClass('menu-open');
-      subMenu.removeAttr('style');
-    }
   })
+}
 
-
-  const btn = $('.sixtyia-header .icon-angle-down');
-
-  if (btn) {
-    btn.each(function () {
-      $(this).click(function () {
-        const windowW = $(window).innerWidth();
-
-        if (windowW <= 992) {
-          $(this).next().stop(true).slideToggle();
-        }
-      })
-    })
+setTimeout(function () {
+  let inputField = document.querySelector('.sixtyai-phone input');
+  if (inputField) {
+    inputField.addEventListener('input', function (event) {
+      let inputValue = event.target.value;
+      let sanitizedValue = inputValue.replace(/[^0-9\-+]/g, '');
+      event.target.value = sanitizedValue;
+    });
   }
+}, 300);
 
-
-  const noLink = $('a[href="#"]');
-
-  if (noLink) {
-    noLink.each(function () {
-      $(this).click(function (el) {
-        el.preventDefault();
-      })
-    })
-  }
-
-
-})(jQuery, window, document);
